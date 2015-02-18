e5169fb87232b78d84de90db5ebbcc21fd3a32b4
Semen Martynov <semen.martynov@gmail.com>
2015-02-18 06:08:18 +0300

    [fix] NamedPipeNetworkClient is restored.

--- InterProcessCommunication/NamedPipeNetworkClient/main.cpp
+++ InterProcessCommunication/NamedPipeNetworkClient/main.cpp
@@ -3,41 +3,46 @@
 #include <conio.h>
 #include <tchar.h>
 #include <strsafe.h>
+#include "logger.h"
 
 #define BUFSIZE 512
 
-int _tmain(int argc, TCHAR *argv[])
-{
-	_tprintf(TEXT("Client is started!\n\n"));
+int _tmain(int argc, _TCHAR* argv[]) {
+	//Init log
+	initlog(argv[0]);
+	_tprintf(_T("Client is started!\n\n"));
 
 	HANDLE hPipe = INVALID_HANDLE_VALUE; // Идентификатор канала
 	LPTSTR lpszPipename = TEXT("\\\\192.168.124.235\\pipe\\$$MyPipe$$"); // Имя создаваемого канала Pipe
-	TCHAR chBuf[BUFSIZE]; // Буфер для передачи данных через канал
+	_TCHAR chBuf[BUFSIZE]; // Буфер для передачи данных через канал
 	DWORD readbytes, writebytes; // Число байт прочитанных и переданных
 
 	// Создание SECURITY_ATTRIBUTES и SECURITY_DESCRIPTOR объектов
 	SECURITY_ATTRIBUTES sa;
 	SECURITY_DESCRIPTOR sd;
 	// Инициализация SECURITY_DESCRIPTOR значениями по-умолчанию
-	if (InitializeSecurityDescriptor(&sd, SECURITY_DESCRIPTOR_REVISION) == 0)
-	{
-		_tprintf(TEXT("InitializeSecurityDescriptor failed with error %d\n"), GetLastError());
-		_getch();
-		return -1;
+	if (InitializeSecurityDescriptor(&sd, SECURITY_DESCRIPTOR_REVISION) == 0) {
+		double errorcode = GetLastError();
+		writelog(_T("InitializeSecurityDescriptor failed, GLE = %d"), errorcode);
+		_tprintf(_T("InitializeSecurityDescriptor failed, GLE = %d\n"), errorcode);
+		closelog();
+		exit(2);
 	}
 	// Установка поля DACL в SECURITY_DESCRIPTOR в NULL
-	if (SetSecurityDescriptorDacl(&sd, TRUE, NULL, FALSE) == 0)
-	{
-		_tprintf(TEXT("SetSecurityDescriptorDacl failed with error %d\n"), GetLastError());
-		_getch();
-		return -1;
+	if (SetSecurityDescriptorDacl(&sd, TRUE, NULL, FALSE) == 0)	{
+		double errorcode = GetLastError();
+		writelog(_T("SetSecurityDescriptorDacl failed, GLE = %d"), errorcode);
+		_tprintf(_T("SetSecurityDescriptorDacl failed, GLE = %d\n"), errorcode);
+		closelog();
+		exit(3);
 	}
 	// Установка SECURITY_DESCRIPTOR в структуре SECURITY_ATTRIBUTES
 	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
 	sa.lpSecurityDescriptor = &sd;
 	sa.bInheritHandle = FALSE; //запрещение наследования
 
-	_tprintf(TEXT("Try to use WaitNamedPipe...\n"));
+	writelog(_T("Try to use WaitNamedPipe..."));
+	_tprintf(_T("Try to use WaitNamedPipe...\n"));
 	// Пытаемся открыть именованный канал, если надо - ожидаем его освобождения
 	while (1)
 	{
@@ -57,24 +62,27 @@ int _tmain(int argc, TCHAR *argv[])
 			break;
 
 		// Выход, если ошибка связана не с занятым каналом. 
-		if (GetLastError() != ERROR_PIPE_BUSY)
-		{
-			_tprintf(TEXT("Could not open pipe. GLE=%d\n"), GetLastError());
-			_getch();
-			return -1;
+		if (GetLastError() != ERROR_PIPE_BUSY) {
+			double errorcode = GetLastError();
+			writelog(_T("Could not open pipe, GLE = %d"), errorcode);
+			_tprintf(_T("Could not open pipe, GLE = %d\n"), errorcode);
+			closelog();
+			exit(4);
 		}
 
 		// Если все каналы заняты, ждём 20 секунд 
-		if (!WaitNamedPipe(lpszPipename, 20000))
-		{
-			_tprintf(TEXT("Could not open pipe: 20 second wait timed out."));
-			_getch();
-			return -1;
+		if (!WaitNamedPipe(lpszPipename, 20000)) {
+			double errorcode = GetLastError();
+			writelog(_T("Could not open pipe: 20 second wait timed out, GLE = %d"), errorcode);
+			_tprintf(_T("ould not open pipe: 20 second wait timed out, GLE = %d\n"), errorcode);
+			closelog();
+			exit(5);
 		}
 	}
 
 	// Выводим сообщение о создании канала
-	_tprintf(TEXT("Successfully connected!\n\nInput message...\n"));
+	writelog(_T("Successfully connected"));
+	_tprintf(_T("Successfully connected!\n\nInput message...\n"));
 	// Цикл обмена данными с серверным процессом
 	while (1)
 	{
@@ -82,19 +90,24 @@ int _tmain(int argc, TCHAR *argv[])
 		_tprintf(TEXT("cmd>"));
 		// Вводим текстовую строку
 		_fgetts(chBuf, BUFSIZE, stdin);
+		writelog(_T("Inpust string is %s"), chBuf);
 		// Передаем введенную строку серверному процессу в качестве команды
-		if (!WriteFile(hPipe, chBuf, (lstrlen(chBuf) + 1)*sizeof(TCHAR), &writebytes, NULL))
-		{
-			_tprintf(TEXT("connection refused\n"));
+		if (!WriteFile(hPipe, chBuf, (lstrlen(chBuf) + 1)*sizeof(TCHAR), &writebytes, NULL)) {
+			double errorcode = GetLastError();
+			writelog(_T("Connection refused, GLE = %d"), errorcode);
+			_tprintf(_T("Connection refused, GLE = %d\n"), errorcode);
 			break;
 		}
 		// Получаем эту же команду обратно от сервера
-		if (ReadFile(hPipe, chBuf, BUFSIZE*sizeof(TCHAR), &readbytes, NULL))
-			_tprintf(TEXT("Received from server: %s\n"), chBuf);
+		if (ReadFile(hPipe, chBuf, BUFSIZE*sizeof(TCHAR), &readbytes, NULL)) {
+			writelog(_T("Received from server: %s\n"), chBuf);
+			_tprintf(_T("Received from server: %s\n"), chBuf);
+		}
 		// Если произошла ошибка, выводим ее код и завершаем работу приложения
 		else {
-			_tprintf(TEXT("ReadFile: Error %ld\n"), GetLastError());
-			_getch();
+			double errorcode = GetLastError();
+			writelog(_T("ReadFile: Error %ld"), errorcode);
+			_tprintf(_T("ReadFile: Error %ld\n"), errorcode);
 			break;
 		}
 		// В ответ на команду "exit" завершаем цикл обмена данными с серверным процессом
@@ -105,6 +118,7 @@ int _tmain(int argc, TCHAR *argv[])
 	// Закрываем идентификатор канала
 	CloseHandle(hPipe);
 
+	closelog();
 	_tprintf(TEXT("Press ENTER to terminate connection and exit\n"));
 	_getch();
 	return 0;
